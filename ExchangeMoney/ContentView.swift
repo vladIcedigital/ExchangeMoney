e33870f41752706606db2fcbba8a9d4ea0bb5072
Vlad Valkov <vlad@icedigital.ru>
2020-06-15 17:17:25 +0300

    Initial Commit

---
+++ ExchangeMoney/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExchangeMoney
+//
+//  Created by Влад Вальков on 15.06.2020.
+//  Copyright © 2020 Vladislav. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
